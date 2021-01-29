037d22398d3e3787237adb91b6e16161b45cf036
Vanildo Vanni <vanildov@br.ibm.com>
2021-01-29 13:47:21 -0300

    Adjustment to recognize fuzzy match settings in entities

--- wa_cli/commands/helpers/cfg.py
+++ wa_cli/commands/helpers/cfg.py
@@ -20,7 +20,7 @@ WAW_FOLDER = 'waw'
 READONLY_SERVICES = 'readonly_services.txt'
 MAIN_BRANCH = 'main_branch.txt'
 
-GIT_WAW = ('https://github.com/xverges/watson-assistant-workbench.git', '8f1f8e3')
+GIT_WAW = ('https://github.com/vanidlo/watson-assistant-workbench.git', '8f1f8e3')
 GIT_WTT = ('https://github.com/cognitive-catalyst/WA-Testing-Tool.git', '25c07b8')
 
 _cache = {'project_folder': '',
